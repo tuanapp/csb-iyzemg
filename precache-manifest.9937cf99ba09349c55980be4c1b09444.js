ca47cb53351180b731b9064fea9e6f542c3ad59d
Code Sandbox <hello@codesandbox.io>
2022-02-26 12:01:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9937cf99ba09349c55980be4c1b09444.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a470ee8ba276d1e0b1e49095d1a7919",
+    "url": "https://tuanapp.github.io/csb-iyzemg/index.html"
+  },
+  {
+    "revision": "606e9a89ed78b87b9b80",
+    "url": "https://tuanapp.github.io/csb-iyzemg/static/js/2.8e632369.chunk.js"
+  },
+  {
+    "revision": "9986b5f35aabeed073dc",
+    "url": "https://tuanapp.github.io/csb-iyzemg/static/js/main.3af263b9.chunk.js"
+  },
+  {
+    "revision": "7530157190ded3e08806",
+    "url": "https://tuanapp.github.io/csb-iyzemg/static/js/runtime~main.d50825cf.js"
+  }
+]);
